b3457be71d504e1db3d35b690f89ee58ce289869
Aria Askaryar <74391255+aaskaryar@users.noreply.github.com>
2023-02-10 11:22:00 -0800

    Update trash_checker.py

--- trash_checker.py
+++ trash_checker.py
@@ -5,17 +5,15 @@ import pandas as pd
 from pandas import DataFrame
 
 trash = pd.read_excel("C:/Users/GisUser/Desktop/Aria/TRASH-TEMPLATE.xlsx", sheet_name='SiteInformation')
-trash.columns = trash.columns.str.lower()
+#trash.columns = trash.columns.str.lower()
 
 #To test if Start time and end time is correct True and False
 badtime = trash.StartTime > trash.EndTime
 
 goodtime = trash.StartTime < trash.EndTime
 
-#Shows all rows which either has good rows which means start time is before end time 
-#or
-#shows all the bad rows which is where start time is after end time
+#testing to see good rows where start is before end
+goodrows = trash[trash['StartTime'] < trash['EndTime']].index.tolist()
 
-badrows = trash[trash['StartTime'] > trash['EndTime']]
-
-goodrows = trash[trash['StartTime'] < trash['EndTime']]
+#checker, for if start time is after end time
+badrows = trash[trash['StartTime'] > trash['EndTime']].index.tolist()
